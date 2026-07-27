6337a24e7a3f29e0ca010e7cb462116086aed5bc
Victoria Monteiro <victoria.vrsm@gmail.com>
2026-07-26 23:00:21 -0300

    Mudei o nome da aluna para Sara Cruz

--- src/boletim/sistema/java/sistemaBoletim.java
+++ src/boletim/sistema/java/sistemaBoletim.java
@@ -5,10 +5,10 @@ public class sistemaBoletim {
         //  Objetivo: Criar um programa que calcula a média de um aluno
         //  e diz se ele atingiu a nota mínima para passar de ano.
 
-        String nome = "Victória";
-        double  notaDaProva01 = 10.0;
-        double  notaDaProva02 = 7.0;
-        double  notaDaProva03 = 5.5;
+        String nome = "Sara Cruz";
+        double  notaDaProva01 = 9.0;
+        double  notaDaProva02 = 6.0;
+        double  notaDaProva03 = 9.0;
         double media = (notaDaProva01 + notaDaProva02 + notaDaProva03) / 3;
         boolean isAprovado = media >= 7.0;
         String resultado = "A aluna " + nome + " tirou as notas " + notaDaProva01 + ", " + notaDaProva02 + " e "
